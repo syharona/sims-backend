3163c7d54a33bfa2451e2f0eaaf65d5233ab8580
user <user@MacBook-Pro-de-HS.local>
2024-08-25 01:37:36 +0100

    Added neon

---
+++ src/admin/app.js
@@ -0,0 +1,10 @@
+export default {
+      translations: {
+        en: {
+          "app.components.LeftMenu.navbrand.title": "Simel Dashboard",
+          "app.components.LeftMenu.navbrand.workplace": "...",
+        },
+    },
+    bootstrap(app) {
+    },
+  };
